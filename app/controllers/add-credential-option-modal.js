20558bd3170d32fb7003455e78a790265100514d
rod <rodz@rod.rod>
2015-05-16 20:08:31 -0400

    add credential option func

--- app/controllers/add-credential-option-modal.js
+++ app/controllers/add-credential-option-modal.js
@@ -10,14 +10,14 @@ export default Ember.Controller.extend({
 	},
 
 	actions:{
-		addCredentialGroup: function(){
+		addCredentialOption: function(){
 			var self = this;
 			var model = self.get('model');
 			var data = self.getProperties('name');
 
 			console.log(data);
 			
-			$.post('http://localhost:8000/api/v1/'+model.company_slug+'/credential/add-credential-group', data).then(function(response){
+			$.post('http://localhost:8000/api/v1/'+model.company_slug+'/credential/add-credential-option', data).then(function(response){
 				
 			});
 
